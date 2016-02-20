e9212a5760e8110dc789c11e55246df3613ab711
Alex Taylor <alexmctaylorpants@gmail.com>
2016-02-19 16:36:58 -0800

    Prepare for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.join(__dirname, 'build'),
-    publicPath: '/assets/',
     filename: 'bundle.js'},
   module: {
     loaders: [
